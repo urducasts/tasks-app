dce60769ff86e98c0c8a86e5e46d4b62ddb1a8a8
Muhammad Ahsan Abrar <iahsanabrar@gmail.com>
2017-04-26 14:45:52 +0500

    Lesson 2

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -32,11 +32,9 @@
 			<div class="col-md-12">
 				<h2 class="mb-3">All Tasks</h2>
 				<ul class="list-group">
-					<li class="list-group-item">Cras justo odio</li>
-					<li class="list-group-item">Dapibus ac facilisis in</li>
-					<li class="list-group-item">Morbi leo risus</li>
-					<li class="list-group-item">Porta ac consectetur ac</li>
-					<li class="list-group-item">Vestibulum at eros</li>
+					@foreach($tasks as $task)
+						<li class="list-group-item">{{ $task->title }}</li>
+					@endforeach
 				</ul>
 			</div>
 		</div>
